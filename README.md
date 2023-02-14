a88857d82f2c475334c9bbdb2567fdd120c78867
IgorBosin <ibosin@yandex.ru>
2023-02-14 12:52:55 +0300

    microtasks filter

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 1. create
 2. microtasks map
 3. microtasks button
+4. microtasks filter
 
 
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React, {useState} from 'react';
-import Button from "./components/Button";
+import NewComponent from "./NewComponent";
 
-type moneyType = {
+export type filterType = 'all' | 'rub' | 'dol'
+
+export type moneyType = {
     banknots: string
     value: number
     number: string
@@ -9,61 +11,30 @@ type moneyType = {
 
 const App = () => {
 
-        const [money, setMoney] = useState<moneyType[]>([
-            {banknots: 'Dollars', value: 100, number: ' a1234567890'},
-            {banknots: 'Dollars', value: 50, number: ' z1234567890'},
-            {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
-            {banknots: 'Dollars', value: 100, number: ' e1234567890'},
-            {banknots: 'Dollars', value: 50, number: ' c1234567890'},
-            {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
-            {banknots: 'Dollars', value: 50, number: ' x1234567890'},
-            {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
-        ])
+    const [money, setMoney] = useState<moneyType[]>([
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+    ])
 
-        function button1Foo(name: string, age: number) {
-            console.log(name, age)
-        }
+    let [filter, setFilter] = useState<filterType>('all')
 
-        function button2Foo(name: string) {
-            console.log(name)
-        }
+    let filterMoney = [...money]
+    if (filter === 'rub') filterMoney = money.filter(filt => filt.banknots === 'RUBLS')
+    if (filter === 'dol') filterMoney = money.filter(filt => filt.banknots === 'Dollars')
 
-        function stupidButton() {
-            console.log('stupid button')
-        }
-
-        let moneyFilter: moneyType[] = [...money]
-
-        let [filt, setFilt] = useState('all')
-        if (filt === 'Dollars') {
-            moneyFilter = money.filter(el => el.banknots === 'Dollars')
-        }
-        if (filt === 'RUBLS') {
-            moneyFilter = money.filter(el => el.banknots === 'RUBLS')
-        }
-
-        function changeBanknots(curr:string) {
-            setFilt(filt=curr)
-        }
-
-        return (
-            <div>
-                {moneyFilter.map(el => {
-                    return (
-                        <div>
-                            {el.value} {el.banknots}
-                        </div>
-                    )
-                })}
-                <Button name={'chanel1'} callBack={() => button1Foo('igor', 28)}/>
-                <Button name={'chanel2'} callBack={() => button2Foo('masha')}/>
-                <Button name={'stupid button'} callBack={stupidButton}/>
-                <Button name={'dollar'} callBack={()=>changeBanknots('Dollars')}/>
-                <Button name={'rub'} callBack={()=>changeBanknots('RUBLS')}/>
-                <Button name={'all'} callBack={()=>changeBanknots('all')}/>
-            </div>
-        );
+    const filtredBanknots = (curr: filterType) => {
+        setFilter(curr)
     }
-;
+
+    return (
+        <NewComponent money={filterMoney} filtred={filtredBanknots}/>
+    );
+};
 
 export default App;
